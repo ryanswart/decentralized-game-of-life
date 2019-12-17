2d8513b580f82723f1582f701a45e3c09a6c2b57
Claudiu Filip <claudiucelfilip@gmail.com>
2019-12-17 14:48:15 +0200

    chore: add contract-deployer script and env variables

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,14 +1,21 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
-
+const path = require("path");
+const webpack = require("webpack");
+console.log("process.env.CONTRACT_ID", process.env.CONTRACT_ID);
 module.exports = {
   entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
+    filename: "index.js"
   },
   mode: "development",
   plugins: [
-    new CopyWebpackPlugin(['index.html'])
-  ],
+    new CopyWebpackPlugin(["index.html"]),
+    new webpack.DefinePlugin({
+      "process.env.WAVELET_API_URL": JSON.stringify(
+        process.env.WAVELET_API_URL
+      ),
+      "process.env.CONTRACT_ID": JSON.stringify(process.env.CONTRACT_ID)
+    })
+  ]
 };
